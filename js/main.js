8b1956a2f9f82e524c6a2abb949e069d5620c8f1
Toroshchin <fedotina107@gmail.com>
2022-08-22 22:34:02 +0300

    add function burgerBtn

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 const nav = document.querySelector('.header-nav')
 const navBtn = document.querySelector('.header-nav-button')
 const navBtnImg = document.querySelector('.header-nav-button__img')
+const headerItem = document.querySelectorAll('.header-nav__item')
 
-navBtn.onclick = () => {
+function burgerBtn() {
   if (nav.classList.toggle('open')) {
     navBtnImg.src = './img/icons/nav-close.svg'
   } else {
     navBtnImg.src = './img/icons/nav-open.svg'
+  }
+}
+
+navBtn.onclick = () => {
+  burgerBtn()
+}
+console.log(headerItem.length)
+
+for (let i = 0; i < headerItem.length; i++) {
+  headerItem[i].onclick = () => {
+    burgerBtn()
   }
 }
